35eb40c9aa9ea88decfb9acfde4f7952d39ba5d6
MicahThGreat <97547182+MicahThGreat@users.noreply.github.com>
2024-07-09 08:32:30 -0500

    create first function

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,8 @@
+function Message() {
+  //JSX: JavaScript XML
+  const name = "Isaac";
+  if (name == "Isaac") return <h1>Hello {name}</h1>;
+  return <h1>Hello Micah</h1>;
+}
+
+export default Message;
